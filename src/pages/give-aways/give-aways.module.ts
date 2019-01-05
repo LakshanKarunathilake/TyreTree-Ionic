6e7f0d07e7755d61430345432323b4a9a6953695
lakshankarunathilake <manualsplash@gmail.com>
2019-01-05 10:47:01 +0530

    Give Away page implmented

--- src/pages/give-aways/give-aways.module.ts
+++ src/pages/give-aways/give-aways.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { GiveAwaysPage } from './give-aways';
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { GiveAwaysPage } from "./give-aways";
+import {
+  MatInputModule,
+  MatSelectModule,
+  MatAutocompleteModule,
+  MatExpansionModule,
+  MatButtonModule,
+  MatListModule,
+  MatDividerModule
+} from "@angular/material";
 
 @NgModule({
-  declarations: [
-    GiveAwaysPage,
-  ],
+  declarations: [GiveAwaysPage],
   imports: [
     IonicPageModule.forChild(GiveAwaysPage),
-  ],
+    MatInputModule,
+    MatSelectModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule
+  ]
 })
 export class GiveAwaysPageModule {}
--- src/pages/give-aways/give-aways.ts
+++ src/pages/give-aways/give-aways.ts
@@ -1,5 +1,15 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  Loading,
+  LoadingController,
+  AlertController,
+  Form
+} from "ionic-angular";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 /**
  * Generated class for the GiveAwaysPage page.
@@ -10,16 +20,185 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-give-aways',
-  templateUrl: 'give-aways.html',
+  selector: "page-give-aways",
+  templateUrl: "give-aways.html"
 })
 export class GiveAwaysPage {
+  loading: Loading;
+  tyreHouseOptions: string[];
+  tyreHouses: string[] = [];
+  tyreNumberOptions: string[];
+  tyreNumbers: string[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  giveAwayForm: FormGroup;
+
+  controllers = {
+    tyre: {
+      collectionName: "Tyres",
+      elementName: "tyreNumber",
+      formController: "tyreNumbers"
+    },
+    tyreHouse: {
+      collectionName: "TyreHouse",
+      elementName: "name",
+      formController: "tyreHouses"
+    }
+  };
+
+  selectedTyres = {
+    firstDag: [],
+    secondDag: [],
+    thirdDag: [],
+    noGuarantee: []
+  };
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private afs: AngularFirestore,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private fb: FormBuilder
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GiveAwaysPage');
+    console.log("ionViewDidLoad GiveAwaysPage");
   }
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.giveAwayForm = this.fb.group({
+      tyreHouses: "",
+      tyreNumbers: "",
+      purpose: ""
+    });
+    this.afs
+      .collection("TyreHouses")
+      .valueChanges()
+      .subscribe(res => {
+        res.forEach(element => {
+          this.tyreHouses.push(element["name"]);
+        });
+        this.tyreHouseOptions = this.tyreHouses;
+        this.giveAwayForm.get("tyreHouses").valueChanges.subscribe(res => {
+          this.tyreHouseOptions = this._filter(res, this.tyreHouses);
+        });
+      });
+    this.afs
+      .collection("Tyres")
+      .valueChanges()
+      .subscribe(res => {
+        res.forEach(element => {
+          this.tyreNumbers.push(element["tyreNumber"]);
+        });
+        this.tyreNumberOptions = this.tyreHouses;
+        this.giveAwayForm.get("tyreNumbers").valueChanges.subscribe(res => {
+          this.tyreNumberOptions = this._filter(res, this.tyreNumbers);
+        });
+      });
+  }
+
+  private _filter(value: string, options: string[]): string[] {
+    if (value === null) {
+      value = "";
+    }
+    const filterValue = value.toLowerCase();
+    return options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  addTyreToCategory() {
+    const { tyreNumbers, purpose } = this.giveAwayForm.value;
+    console.log("clicked", purpose);
+    switch (purpose) {
+      case "1 Dagging":
+        this.selectedTyres.firstDag.push(tyreNumbers);
+        break;
+      case "2 Dagging":
+        this.selectedTyres.secondDag.push(tyreNumbers);
+        break;
+      case "3 Dagging":
+        this.selectedTyres.thirdDag.push(tyreNumbers);
+        break;
+      case "No guarantee":
+        this.selectedTyres.noGuarantee.push(tyreNumbers);
+        break;
+
+      default:
+        break;
+    }
+    console.log("this.selectedTyres :", this.selectedTyres);
+  }
+
+  onFocusEvent(name) {
+    // const { collectionName, elementName, formController } = this.controllers[
+    //   name
+    // ];
+    // this.afs
+    //   .collection(collectionName)
+    //   .valueChanges()
+    //   .subscribe(res => {
+    //     res.forEach(element => {
+    //       this.tyreNumbers.push(element[elementName]);
+    //     });
+    //     this.tyreNumberOptions = this.tyreHouses;
+    //     this.giveAwayForm.get(formController).valueChanges.subscribe(res => {
+    //       this.tyreNumberOptions = this._filter(res, this.tyreNumbers);
+    //     });
+    //   });
+  }
+
+  onFormSubmission() {
+    console.log("form Submitted");
+    const tyreHouseName = this.giveAwayForm.value.tyreHouses;
+    const purpose = this.giveAwayForm.value.purpose;
+    Object.keys(this.selectedTyres).forEach(key => {
+      console.log("key", key);
+      this.selectedTyres[key].forEach(element => {
+        console.log("element :", element);
+        const timeStamp = new Date();
+        const dueDate = timeStamp;
+        dueDate.setMonth(timeStamp.getMonth() + 3);
+        this.saveGiveAwayToFireStore(
+          {
+            tyreNumber: element,
+            tyreHouse: tyreHouseName,
+            purpose: purpose,
+            givenDate: timeStamp.toString(),
+            dueDate: dueDate,
+            receivedStatus: false
+          },
+          timeStamp
+        );
+      });
+    });
+  }
+
+  saveGiveAwayToFireStore(value, timeStamp) {
+    this.afs
+      .collection("GiveAways")
+      .doc(timeStamp.toString())
+      .set(value);
+  }
+
+  presentLoading(message: string) {
+    this.loading = this.loadingCtrl.create({
+      spinner: "crescent",
+      content: message
+    });
+    this.loading.present();
+  }
+
+  hideLoading() {
+    this.loading.dismiss();
+  }
+
+  presentAlert(message) {
+    this.alertCtrl
+      .create({
+        message: message,
+        buttons: ["Dismiss"]
+      })
+      .present();
+  }
 }
