3bd7be9327c18bc4c667f277384547598963460f
lakshankarunathilake <manualsplash@gmail.com>
2019-01-11 21:35:49 +0530

    Recieving Page recieve functionality added

--- src/pages/receivings/receivings.ts
+++ src/pages/receivings/receivings.ts
@@ -27,6 +27,7 @@ export class ReceivingsPage {
   myInput;
   loading: Loading;
   oldestGiveAways = [];
+  allGiveAways = [];
 
   constructor(
     public navCtrl: NavController,
@@ -39,6 +40,7 @@ export class ReceivingsPage {
       .collection("GiveAways", ref =>
         ref
           .where("dueDate", "<", new Date())
+          // .where("receivedStatus", "==", false)
           .orderBy("dueDate", "asc")
           .limit(10)
       )
@@ -57,8 +59,72 @@ export class ReceivingsPage {
           if (deal.receivedStatus === false)
             this.oldestGiveAways.push({ id: el.payload.doc.id, ...deal });
         });
-        console.log("this.oldestGiveAways", this.oldestGiveAways);
+        console.log("this.allgiveaways", this.allGiveAways);
       });
+
+    this.afs
+      .collection("GiveAways", ref => ref.orderBy("givenDate", "asc"))
+      .snapshotChanges()
+      .subscribe(val => {
+        console.log("val", val);
+        val.forEach(element => {
+          let deal = element.payload.doc.data() as TyreAway;
+          deal.dueDate = this.formatDate(
+            new Date(deal.dueDate["seconds"] * 1000)
+          );
+          deal.givenDate = this.formatDate(
+            new Date(deal.givenDate["seconds"] * 1000)
+          );
+          if (deal.receivedStatus === false)
+            this.allGiveAways.push({ id: element.payload.doc.id, ...deal });
+        });
+        console.log("allGiveAways :", this.allGiveAways);
+      });
+  }
+
+  receiveTheTyre(id: string, number: string, purpose: string) {
+    console.log("id", id, "number", number);
+    this.updateTyreReceivedStatus(id, number, purpose);
+  }
+
+  updateTyreReceivedStatus(id: string, number: string, purpose: string) {
+    this.afs
+      .collection("GiveAways")
+      .doc(id)
+      .update({ receivedStatus: true })
+      .then(() => {
+        this.changeTyreAvailability(number, purpose);
+      });
+  }
+
+  changeTyreAvailability(number: string, purpose: string) {
+    this.selectTheNextStatus(purpose);
+    const successMessage = `Details saved Successfully 
+    <br>
+    <br>      
+    <div align="center"> <img src="../assets/imgs/success.png" weight="50px" height="50px"></div>
+   `;
+    this.afs
+      .collection("Tyres")
+      .doc(number)
+      .update({ availability: "stock", purpose })
+      .then(() => this.presentAlert(successMessage));
+  }
+
+  selectTheNextStatus(purpose: string) {
+    switch (purpose) {
+      case "firstDag":
+        return "1 Dagged";
+      case "secondDag":
+        return "2 Dagged";
+      case "thirdDag":
+        return "3 Dagged";
+      case "thirdDag":
+        return "No Guarantee";
+
+      default:
+        break;
+    }
   }
 
   formatDate(timeStamp: Date) {
@@ -81,10 +147,11 @@ export class ReceivingsPage {
 
   onCancel(event) {}
   presentLoading(message: string) {
-    this.loading = this.loadingCtrl.create({
-      spinner: "crescent",
-      content: message
-    });
+    if (this.loading !== undefined)
+      this.loading = this.loadingCtrl.create({
+        spinner: "crescent",
+        content: message
+      });
     this.loading.present();
   }
 
